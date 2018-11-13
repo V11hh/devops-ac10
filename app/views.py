9c93830e2188d0677e8a03460b164eff9ae385ed
V11hh <42079944+V11hh@users.noreply.github.com>
2018-11-13 12:04:26 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -72,7 +72,7 @@ def cadastro_vestibulares(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/cadastro_vestibulares.html’,
+        'app/cadastro_vestibulares.html',
         context_instance = RequestContext(request,
         {
             'title':'Cadastro de vestibulares’,
